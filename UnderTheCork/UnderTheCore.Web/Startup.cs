ad98165ee5d3888a1104b09ecd714376defe0184
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-09-28 18:51:21 +0300

    Initial project structure added.

---
+++ UnderTheCork/UnderTheCore.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UnderTheCore.Web.Startup))]
+namespace UnderTheCore.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
