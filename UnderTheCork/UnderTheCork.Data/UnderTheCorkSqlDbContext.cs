5e8de50ef40ab7727eabba23d44530494761faa3
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-09-30 21:26:48 +0300

    Ninject installed and configured.

--- UnderTheCork/UnderTheCork.Data/UnderTheCorkSqlDbContext.cs
+++ UnderTheCork/UnderTheCork.Data/UnderTheCorkSqlDbContext.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
-
+using System;
+using System.Data.Entity;
+using System.Linq;
 using UnderTheCork.Data.Models;
+using UnderTheCork.Data.Models.Contracts;
 
 namespace UnderTheCork.Data
 {
@@ -14,6 +17,32 @@ namespace UnderTheCork.Data
         public static UnderTheCorkSqlDbContext Create()
         {
             return new UnderTheCorkSqlDbContext();
+        }
+
+        public override int SaveChanges()
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChanges();
+        }
+
+        private void ApplyAuditInfoRules()
+        {
+            foreach (var entry in
+                this.ChangeTracker.Entries()
+                    .Where(
+                        e =>
+                        e.Entity is IAuditable && ((e.State == EntityState.Added) || (e.State == EntityState.Modified))))
+            {
+                var entity = (IAuditable)entry.Entity;
+                if (entry.State == EntityState.Added && entity.CreatedOn == default(DateTime))
+                {
+                    entity.CreatedOn = DateTime.Now;
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.Now;
+                }
+            }
         }
     }
 }
