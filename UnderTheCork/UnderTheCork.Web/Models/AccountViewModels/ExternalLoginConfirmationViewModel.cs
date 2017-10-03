3cea629b9a971d5e18484cfb5b23998addc9a2e4
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-10-03 15:42:55 +0300

    Login with username and password implemented instead email and password.

--- UnderTheCork/UnderTheCork.Web/Models/AccountViewModels/ExternalLoginConfirmationViewModel.cs
+++ UnderTheCork/UnderTheCork.Web/Models/AccountViewModels/ExternalLoginConfirmationViewModel.cs
@@ -5,6 +5,10 @@ namespace UnderTheCork.Web.Models.AccountViewModels
     public class ExternalLoginConfirmationViewModel
     {
         [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
         [Display(Name = "Email")]
         public string Email { get; set; }
     }
