78004720e1af77a5a39816bb0893a363453523d6
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-10-05 17:16:55 +0300

    Country data model added.

---
+++ UnderTheCork/UnderTheCork.Data.Models/Country.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using UnderTheCork.Common;
+using UnderTheCork.Data.Models.Abstracts;
+
+namespace UnderTheCork.Data.Models
+{
+    public class Country : BaseDataModel
+    {
+        private ICollection<Region> regions;
+
+        public Country()
+        {
+            this.regions = new HashSet<Region>();
+        }
+
+        [Required]
+        [Index(IsUnique = true)]
+        [MinLength(DataModelsConstants.MinLengthCountryName)]
+        [MaxLength(DataModelsConstants.MaxLengthCountryName)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Region> Regions
+        {
+            get { return this.regions; }
+            set { this.regions = value; }
+        }
+    }
+}
