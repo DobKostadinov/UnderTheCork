3040fb7a71c777e01b83e6d37da95fcae54d6e46
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-10-05 17:17:48 +0300

    Region data model added.

---
+++ UnderTheCork/UnderTheCork.Data.Models/Region.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using UnderTheCork.Common;
+using UnderTheCork.Data.Models.Abstracts;
+
+namespace UnderTheCork.Data.Models
+{
+    public class Region : BaseDataModel
+    {
+        private ICollection<Winery> wineries;
+
+        public Region()
+        {
+            this.wineries = new HashSet<Winery>();
+        }
+
+        [Required]
+        [Index(IsUnique = true)]
+        [MinLength(DataModelsConstants.MinLengthRegionName)]
+        [MaxLength(DataModelsConstants.MaxLengthRegionName)]
+        public string Name { get; set; }
+
+        public Guid CountryId { get; set; }
+
+        public virtual Country Country { get; set; }
+
+        public virtual ICollection<Winery> Wineries
+        {
+            get { return this.wineries; }
+            set { this.wineries = value; }
+        }
+    }
+}
