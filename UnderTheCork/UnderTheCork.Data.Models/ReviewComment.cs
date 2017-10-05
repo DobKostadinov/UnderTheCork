6901716d6f29535f0ede999d4fd0fcb85772aafb
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-10-05 17:20:32 +0300

    ReviewComment data model added.

---
+++ UnderTheCork/UnderTheCork.Data.Models/ReviewComment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+using UnderTheCork.Common;
+using UnderTheCork.Data.Models.Abstracts;
+
+namespace UnderTheCork.Data.Models
+{
+    public class ReviewComment : BaseDataModel
+    {
+        [Required]
+        [MinLength(DataModelsConstants.MinLengthReviewContent)]
+        [MaxLength(DataModelsConstants.MaxLengthReviewContent)]
+        public string  Content { get; set; }
+
+        public string UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+        public Guid WineReviewId { get; set; }
+
+        public virtual WineReview WineReview { get; set; }
+    }
+}
