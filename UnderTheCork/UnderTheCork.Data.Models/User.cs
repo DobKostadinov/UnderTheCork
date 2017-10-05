56d8e77c81b4d0b859ba4bc8779e963b025e3669
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-10-05 17:18:26 +0300

    User data model updated.

--- UnderTheCork/UnderTheCork.Data.Models/User.cs
+++ UnderTheCork/UnderTheCork.Data.Models/User.cs
@@ -1,13 +1,52 @@
-﻿using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+using UnderTheCork.Data.Models.Contracts;
+
 namespace UnderTheCork.Data.Models
 {
-    public class User : IdentityUser
+    public class User : IdentityUser, IAuditable, IDeletable
     {
+        private ICollection<WineReview> reviews;
+        private ICollection<ReviewComment> comments;
+
+        public User()
+        {
+            this.reviews = new HashSet<WineReview>();
+            this.comments = new HashSet<ReviewComment>();
+        }
+
+        [Index]
+        public bool IsDeleted { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? DeletedOn { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? CreatedOn { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? ModifiedOn { get; set; }
+
+        public virtual ICollection<WineReview> Reviews
+        {
+            get { return this.reviews; }
+            set { this.reviews = value; }
+        }
+
+        public virtual ICollection<ReviewComment> Comments
+        {
+            get { return this.comments; }
+            set { this.comments = value; }
+        }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
